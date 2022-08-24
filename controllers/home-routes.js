4fbe2a8a3477b32f2bc09404f9b08484e90a22d9
Morgan Holcomb <morganholcomb2019@gmail.com>
2022-08-23 19:20:39 -0500

    can add post and see login logout depending on which is needed

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -31,7 +31,11 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
       const posts = dbPostData.map(post => post.get({ plain: true }));
 
-      res.render('homepage', { posts });
+      res.render('homepage',
+        {
+          posts,
+          loggedIn: req.session.loggedIn
+        });
     })
     .catch(err => {
       console.log(err);
@@ -102,7 +106,11 @@ router.get('/post/:id', (req, res) => {
       const post = dbPostData.get({ plain: true });
 
       // pass data to template
-      res.render('single-post', { post });
+      res.render('single-post',
+        {
+          post,
+          loggedIn: req.session.loggedIn
+        });
     })
     .catch(err => {
       console.log(err);
